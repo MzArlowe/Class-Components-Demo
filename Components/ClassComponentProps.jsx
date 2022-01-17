2a646f30093dd5fbccda984374d41e49dad5bd85
unknown <bhm727@yahoo.com>
2022-01-17 11:37:17 -0500

    finished module 1.08

--- Components/ClassComponentProps.jsx
+++ Components/ClassComponentProps.jsx
@@ -3,24 +3,59 @@ import React, { Component } from 'react';
 export default class ClassComponentProps extends Component {
     constructor(props) {
         super(props);
-            this.state = { 
+        this.state = {
             fruitBowl: ["Banana", "Apple", "Pear", "Peach", "Kiwi"],
             newFruit: "",
         };
+        this.addFruit = this.addFruit.bind(this);
     }
+
+    addFruit(event) { //this is the event where a user "adds" fruit into the array
+        event.preventDefault();
+        this.setState({
+            fruitBowl: [...this.state.fruitBowl, this.state.newFruit],
+            newFruit: "",
+        });
+    }
+
+    changeHandler(event) {
+        this.setState({ newFruit: event.target.value });
+    }
+
     render() {
         return (
             <div className="main">
                 <div className="mainDiv">
-                    <form>
+                    <form onSubmit={this.addFruit}>
                         <input type="text"
-                        value={this.state.newFruit} />
+                            value={this.state.newFruit}
+                            onChange={(event) => this.changeHandler(event)}
+                        />
                         <button type="submit">Add Fruit</button>
                     </form>
                 </div>
-                
-            
             </div>
-        )
+        );
     }
 }
+
+
+class FruitBowl extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.fruits.map((fruit) => {
+                    return <Fruit passingFruit = {fruit} />
+                })}
+            </div>
+        );
+    }
+}
+
+const Fruit = (props) => {
+    return (
+        <div>
+            <h3>{props.passingFruit}</h3>
+        </div>
+    );
+};
