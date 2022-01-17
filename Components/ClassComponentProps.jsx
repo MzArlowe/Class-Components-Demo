65046e886ac6d4c05756a0c146b46aeb84406459
unknown <bhm727@yahoo.com>
2022-01-17 10:00:28 -0500

    Added two components to jsx file

--- Components/ClassComponentProps.jsx
+++ Components/ClassComponentProps.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class ClassComponentProps extends Component {
+    constructor(props) {
+        super(props);
+            this.state = { 
+            fruitBowl: ["Banana", "Apple", "Pear", "Peach", "Kiwi"],
+            newFruit: "",
+        };
+    }
     render() {
         return (
-            <div>
-
+            <div className="main">
+                <div className="mainDiv">
+                    <form>
+                        <input type="text"
+                        value={this.state.newFruit} />
+                        <button type="submit">Add Fruit</button>
+                    </form>
+                </div>
+                
             
             </div>
         )
